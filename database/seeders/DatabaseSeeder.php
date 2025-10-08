f599a06221012c00b0ee250868cbad96c83e6200
vinxzs <koktaviaaan7@gmail.com>
2025-10-08 10:48:05 +0700

    FIX

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CleanupDuplicateDataSeeder::class,
             DepartemenSupervisorSeeder::class,
             AreaSeeder::class,
             PenanggungJawabSeeder::class,
