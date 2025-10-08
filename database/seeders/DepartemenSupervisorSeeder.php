6270c8e1a04d8a695931e8a4a821b290e48be604
vinxzs <koktaviaaan7@gmail.com>
2025-10-08 09:17:43 +0700

    feat: update seeder with real department data

--- database/seeders/DepartemenSupervisorSeeder.php
+++ database/seeders/DepartemenSupervisorSeeder.php
@@ -13,15 +13,91 @@ class DepartemenSupervisorSeeder extends Seeder
      */
     public function run(): void
     {
-        DepartemenSupervisor::updateOrCreate(
-            ['id' => 1],
+        $supervisors = [
             [
-                'departemen' => 'Default Department',
-                'supervisor' => 'Default Supervisor',
-                'workgroup' => 'WG-DEFAULT',
-                'email' => 'supervisor@siemens.com',
-                'updated_at' => now()
-            ]
-        );
+                'id' => 1,
+                'departemen' => 'LV Assembly',
+                'supervisor' => 'Aris Setiawan',
+                'workgroup' => 'SI EA O AIS JKT MD AS LV',
+                'email' => 'sirjotaro666@gmail.com',
+            ],
+            [
+                'id' => 2,
+                'departemen' => 'LV Box',
+                'supervisor' => 'Rachmad Haryono',
+                'workgroup' => 'SI EA O AIS JKT MF AS LVB',
+                'email' => null,
+            ],
+            [
+                'id' => 3,
+                'departemen' => 'MV Assembly',
+                'supervisor' => 'Helmy Sundani',
+                'workgroup' => 'SI EA O AIS JKT MF AS MV',
+                'email' => null,
+            ],
+            [
+                'id' => 4,
+                'departemen' => 'Prefabrication',
+                'supervisor' => 'Sarifudin Raysan',
+                'workgroup' => 'SI EA O AIS JKT MF PR',
+                'email' => null,
+            ],
+            [
+                'id' => 5,
+                'departemen' => 'Packing',
+                'supervisor' => 'Bayu Putra Trianto',
+                'workgroup' => 'SI EA O AIS JKT MF MS PKG',
+                'email' => null,
+            ],
+            [
+                'id' => 6,
+                'departemen' => 'Tool Store',
+                'supervisor' => 'Joni Rahman',
+                'workgroup' => 'SI EA O AIS JKT MF MS FS',
+                'email' => null,
+            ],
+            [
+                'id' => 7,
+                'departemen' => 'QC LV',
+                'supervisor' => 'Ishak Marthen',
+                'workgroup' => 'SI EA O AIS JKT QC LV',
+                'email' => null,
+            ],
+            [
+                'id' => 8,
+                'departemen' => 'QC MV',
+                'supervisor' => 'Sirad Nova Mihardi',
+                'workgroup' => 'SI EA O AIS JKT QC MV',
+                'email' => null,
+            ],
+            [
+                'id' => 9,
+                'departemen' => 'IQC',
+                'supervisor' => 'Abduh Al Agani',
+                'workgroup' => 'SI EA O AIS JKT QC',
+                'email' => null,
+            ],
+            [
+                'id' => 10,
+                'departemen' => 'LV Module',
+                'supervisor' => 'Hadi Djohansyah',
+                'workgroup' => 'SI EA O AIS JKT MF AS LVM',
+                'email' => null,
+            ],
+            [
+                'id' => 11,
+                'departemen' => 'Warehouse',
+                'supervisor' => 'Suhendra',
+                'workgroup' => 'SI EA O AIS JKT LOG WH',
+                'email' => null,
+            ],
+        ];
+
+        foreach ($supervisors as $supervisorData) {
+            DepartemenSupervisor::updateOrCreate(
+                ['id' => $supervisorData['id']],
+                $supervisorData
+            );
+        }
     }
 }
